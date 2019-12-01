40bff78c6f19c1b80d433f49fae725d0fd01b061
Spencer Vaterlaus <spencervaterlaus@gmail.com>
2019-12-01 14:19:57 -0600

    Rewrite package w/ functional ramda (WIP)

--- index.js
+++ index.js
@@ -1,89 +1,107 @@
-const { _value } = require('./lib/symbols')
+const { Subject } = require('observable-x')
+const { reduce, always, anyPass, pipe, equals, type, ifElse, curry, isNil, map, identity, cond, prop, T, when, call } = require('ramda')
 
-const { isArray } = Array
-const { keys: keysOf } = Object
+const { _value, _subject } = require('./lib/symbols')
 
-const isPOJO = obj => obj === null || typeof obj !== 'object'
-  ? false
-  : Object.getPrototypeOf(obj) === Object.prototype
+const { setPrototypeOf } = Object
 
-const isScalar = val =>
-  val === null ||
-  typeof val === 'string' ||
-  typeof val === 'boolean' ||
-  (typeof val === 'number' && val > -Infinity && val < Infinity)
+const isMappable = pipe(
+  type,
+  anyPass([
+    equals('Array'),
+    equals('Object')
+  ])
+)
 
-const objMap = (obj, transform) => keysOf(obj)
-  .reduce((result, key) => ({ ...result, [key]: transform(obj[key]) }), {})
+const isScalar = pipe(
+  type,
+  anyPass([
+    equals('Number'),
+    equals('String'),
+    equals('Boolean'),
+    equals('Null')
+  ])
+)
 
-const isJSONSerializable = value => {
-  if (isArray(value)) {
-    return value.reduce((result, item) => !result ? result : isJSONSerializable(item), true)
-  }
-  if (isPOJO(value)) {
-    return keysOf(value).reduce((result, key) => !result ? result : isJSONSerializable(value[key]), true)
-  }
-  if (isScalar(value)) {
-    return true
-  }
-  return false
-}
+const isFunction = pipe(
+  type,
+  equals('Function')
+)
+
+const getKeyFromPath = curry((path, value) =>
+  reduce((target, key) => ifElse(
+    isNil,
+    always(value[key]),
+    always(target.get(key))
+  )(target), null)(path)
+)
 
 const reactivePrototype = {
   valueOf () {
-    const value = this[_value]
-    if (isArray(value)) {
-      return value.map(item => item.valueOf())
-    }
-    if (isPOJO(value)) {
-      return objMap(value, prop => prop.valueOf())
-    }
-    return this[_value]
+    return ifElse(
+      isMappable,
+      pipe(prop('valueOf'), call),
+      identity
+    )(this[_value])
   },
   get (path) {
-    if (isArray(path)) {
-      let isFirst = true
-      return path.reduce((result, key) => {
-        const value = isFirst ? this[_value][key] : result.get(key)
-        isFirst = false
-        return value
-      }, null)
-    }
-    if (typeof path === 'string' || typeof path === 'number') {
-      return this[_value][path]
-    }
-    throw new Error(`reactor.get() argument must be an array, string, or number . Received: ${path}`)
+    return cond([
+      [always(isMappable(path)), getKeyFromPath(path)],
+      [always(isScalar(path)), prop(path)],
+      [T, always(undefined)]
+    ])(this[_value])
   },
   update (transform) {
-    const updated = typeof transform === 'function' ? transform(this.valueOf()) : transform
-    if (!isJSONSerializable(updated)) {
-      throw new Error('reactor.update() argument must be JSON-serializable, or a function with a JSON-serializable return value')
-    }
-    this[_value] = updated
+    // 1. only update existing properties based on the input
+    // 2. don't bother updating identical property values
+    // 3. don't add additional properties from the input
+    // 4. update properties missing from the input with null
+    // (use basic type checking?)
+
+    const value = this.valueOf()
+    const transformed = when(isFunction, always(transform(value)))(transform)
+
+    console.log(transformed)
+
+    // this[_value] = cond([
+    //   [isMappable, map(Reactor)]
+    // ])(value)
+
+    // if (isMappable(value)) {
+    //   this[_value] = isFunction(transform) ? map()
+    // } else {
+
+    // }
+    // this[_value] = isFunction(transform)
+    //   ? isMappable(this[_value]) && map(Reactor, this[_value])
+    //   : isScalar(this[_value]) && this[_value]
+
+    // this[_value] = ifElse(
+    //   isFunction,
+    //   always(transform(this.valueOf())),
+    //   transform
+    // )(transform)
     return this[_value]
+  },
+  subscribe (callback) {
+    this[_subject] = when(isNil, Subject(this[_value]))(this[_subject])
+    this[_subject].observe({ next: callback })
   }
 }
 
-const reactive = state => {
+const makeReactive = state => {
   const result = {
-    [_value]: state
+    [_value]: state,
+    [_subject]: null
   }
-
-  Object.setPrototypeOf(result, reactivePrototype)
+  setPrototypeOf(result, reactivePrototype)
   return result
 }
 
-const Reactor = state => {
-  if (isArray(state)) {
-    return reactive(state.map(Reactor))
-  }
-  if (isPOJO(state)) {
-    return reactive(objMap(state, Reactor))
-  }
-  if (isScalar(state)) {
-    return reactive(state)
-  }
-  throw new Error(`Reactor() argument must be JSON-serializable. Received: ${state}`)
-}
+const Reactor = state => cond([
+  [isMappable, pipe(map(Reactor), makeReactive)],
+  [isScalar, makeReactive],
+  [T, always(undefined)]
+])(state)
 
 module.exports = Reactor
