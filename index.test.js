ca5205db924bec94260b4d95c5a72126a554bff8
Spencer Vaterlaus <spencervaterlaus@pop-os.localdomain>
2019-12-05 17:53:51 -0600

    Update test for reactor.update()

--- index.test.js
+++ index.test.js
@@ -97,36 +97,7 @@ describe('reactor.get()', () => {
 })
 
 describe('reactor.update()', () => {
-  test('should return undefined when given an argument that\'s not a string, array, number, boolean, object, or null, or a function that returns a value that\'s not one of those types', () => {
-    const person = { name: 'Spencer', age: 25, hobbies: ['programming', 'eating', 'exercise?'] }
-
-    expect(Reactor(person).update(undefined)).toBe(undefined)
-    expect(Reactor(person).update(() => undefined)).toBe(undefined)
-    expect(Reactor(person).update(new Set())).toBe(undefined)
-    expect(Reactor(person).update(() => new Set())).toBe(undefined)
-    expect(Reactor(person).update(Error)).toBe(undefined)
-    expect(Reactor(person).update(() => Error)).toBe(undefined)
-    expect(Reactor(person).update(Infinity)).toBe(undefined)
-    expect(Reactor(person).update(() => Infinity)).toBe(undefined)
-    expect(Reactor(person).update({ name: 'Spencer', age: 25, hobbies: ['programming', 'eating', new Error()] })).toBe(undefined)
-    expect(Reactor(person).update(() => ({ name: 'Spencer', age: 25, hobbies: ['programming', 'eating', new Error()] }))).toBe(undefined)
-    console.log(Reactor(person).update({}))
-    expect(Reactor(person).update({})).not.toThrow()
-    expect(Reactor(person).update(() => ({}))).not.toThrow()
-    expect(Reactor(person).update([])).not.toThrow()
-    expect(Reactor(person).update(() => [])).not.toThrow()
-    expect(Reactor(person).update(9)).not.toThrow()
-    expect(Reactor(person).update(() => 9)).not.toThrow()
-    expect(Reactor(person).update('foo')).not.toThrow()
-    expect(Reactor(person).update(() => 'foo')).not.toThrow()
-    expect(Reactor(person).update(false)).not.toThrow()
-    expect(Reactor(person).update(() => false)).not.toThrow()
-    expect(Reactor(person).update(null)).not.toThrow()
-    expect(Reactor(person).update(() => null)).not.toThrow()
-    expect(Reactor(person).update({ name: 'Bob', age: 50, hobbies: ['eating', 'sleeping', 'pooping'] })).not.toThrow()
-    expect(Reactor(person).update(() => ({ name: 'Bob', age: 50, hobbies: ['eating', 'sleeping', 'pooping'] }))).not.toThrow()
-  })
-  test('should update the internal value of the reactive item, returning that updated value for reactor.get(path) and reactor.valueOf()', () => {
+  test('should update the value of the reactive item, returning that updated value for reactor.get(path) and reactor.valueOf()', () => {
     const person = { name: 'Spencer', age: 25, hobbies: ['programming', 'eating', 'exercise?'] }
     const reactivePerson = Reactor(person)
 
@@ -135,6 +106,6 @@ describe('reactor.update()', () => {
     reactivePerson.update(p => ({ ...p, name: 'George' }))
 
     expect(reactivePerson.get('name').valueOf()).toBe('George')
-    expect(reactivePerson.get(['hobbies', 0]).valueOf()).toBe('programming')
+    expect(reactivePerson.get('hobbies').valueOf()[0]).toBe('programming')
   })
 })
